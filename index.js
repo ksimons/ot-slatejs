563917cd0e694a346e829195c083d98c10936e51
Kevin Simons <kevin.b.simons@gmail.com>
2018-04-08 10:43:27 +0200

    Implement selection trasformation for text operations

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
 const { Operations, Value } = require('slate');
+const debug = require('debug')('ot-slatejs');
+const textType = require('ot-text');
+
+class Selection {
+  constructor({ anchorPath, anchorOffset, focusPath, focusOffset, isBackward, isFocused }) {
+    this.anchorPath = anchorPath.slice(0);
+    this.anchorOffset = anchorOffset;
+    this.focusPath = focusPath.slice(0);
+    this.focusOffset = focusOffset;
+    this.isBackward = isBackward;
+    this.isFocused = isFocused;
+  }
+
+  get leftPath() {
+    return this.isBackward ? this.focusPath : this.anchorPath;
+  }
+  set leftPath(path) {
+    this[this.isBackward ? 'focusPath' : 'anchorPath'] = path;
+  }
+  get leftOffset() {
+    return this.isBackward ? this.focusOffset : this.anchorOffset;
+  }
+  set leftOffset(offset) {
+    this[this.isBackward ? 'focusOffset' : 'anchorOffset'] = offset;
+  }
+  get rightPath() {
+    return this.isBackward ? this.anchorPath : this.focusPath;
+  }
+  set rightPath(path) {
+    this[this.isBackward ? 'anchorPath' : 'focusPath'] = path;
+  }
+  get rightOffset() {
+    return this.isBackward ? this.anchorOffset : this.focusOffset;
+  }
+  set rightOffset(offset) {
+    this[this.isBackward ? 'anchorOffset' : 'focusOffset'] = offset;
+  }
+}
+
+function arraysEqual(a1, a2) {
+  return a1.length === a2.length && a1.every((value, index) => value === a2[index]);
+}
+
+function transformSelectionTextOperation(selection, op, otTextConversion) {
+  const otSelection = [selection.leftOffset, selection.rightOffset];
+  const otTextOperation = otTextConversion(op);
+  [left, right] = textType.type.transformSelection(otSelection, otTextOperation, false);
+
+  if (arraysEqual(op.path, selection.leftPath)) {
+    selection.leftOffset = left;
+  }
+  if (arraysEqual(op.path, selection.rightPath)) {
+    selection.rightOffset = right;
+  }
+  return selection;
+}
+
+function transformSelectionInsertText(selection, op) {
+  return transformSelectionTextOperation(selection, op, op => [op.offset, op.text]);
+}
+
+function transformSelectionRemoveText(selection, op) {
+  return transformSelectionTextOperation(selection, op, op => [op.offset, { d: op.text.length }]);
+}
 
 module.exports = {
   name: 'slate',
@@ -13,5 +77,19 @@ module.exports = {
   },
 
   transform(op1, op2, side) {
+  },
+
+  transformSelection(selection, ops) {
+    const result = ops.reduce((sel, op) => {
+      switch (op.type) {
+        case 'insert_text':
+          return transformSelectionInsertText(sel, op);
+        case 'remove_text':
+          return transformSelectionRemoveText(sel, op);
+        default:
+          debug(`Unsupported Slate operation type ${op.type}`);
+      }
+    }, new Selection(selection));
+    return Object.assign({}, result);
   },
 };
---
+++ index.test.js
@@ -0,0 +1,327 @@
+const slateType = require('.');
+
+describe('transformSelection', () => {
+  describe('text operations', () => {
+
+    function insertText(offset, path = [0, 0]) {
+      return {
+        type: 'insert_text',
+        path,
+        offset: offset,
+        text: 'hello',
+        marks: [],
+      };
+    }
+
+    function removeText(offset, path = [0, 0]) {
+      return {
+        type: 'remove_text',
+        path,
+        offset: offset,
+        text: 'hello',
+      };
+    }
+
+    function selection(anchorOffset, focusOffset, anchorPath = [0, 0], focusPath = [0, 0]) {
+      const anchorNode = anchorPath[0];
+      const focusNode = focusPath[0];
+      const isBackward = anchorNode === focusNode ? focusOffset < anchorOffset : focusNode < anchorNode;
+      return {
+        anchorPath,
+        anchorOffset,
+        focusPath,
+        focusOffset,
+        isBackward,
+      };
+    }
+
+    describe('insert_text', () => {
+      test('insert before selection position', () => {
+        expect(slateType.transformSelection(
+          selection(10, 10),
+          [insertText(5)],
+        )).toEqual(selection(15, 15));
+      });
+      test('insert at selection position', () => {
+        expect(slateType.transformSelection(
+          selection(10, 10),
+          [insertText(10)],
+        )).toEqual(selection(10, 10));
+      });
+      test('insert after selection position', () => {
+        expect(slateType.transformSelection(
+          selection(10, 10),
+          [insertText(15)],
+        )).toEqual(selection(10, 10));
+      });
+      test('insert before selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [insertText(5)],
+        )).toEqual(selection(15, 25));
+      });
+      test('insert at selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [insertText(10)],
+        )).toEqual(selection(10, 25));
+      });
+      test('insert between selection range anchor and focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [insertText(12)],
+        )).toEqual(selection(10, 25));
+      });
+      test('insert at selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [insertText(20)],
+        )).toEqual(selection(10, 20));
+      });
+      test('insert after selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [insertText(25)],
+        )).toEqual(selection(10, 20));
+      });
+      test('insert before backwards selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [insertText(8)],
+        )).toEqual(selection(25, 15));
+      });
+      test('insert at backwards selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [insertText(10)],
+        )).toEqual(selection(25, 10));
+      });
+      test('insert between backwards selection range focus and anchor', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [insertText(12)],
+        )).toEqual(selection(25, 10));
+      });
+      test('insert at backwards selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [insertText(20)],
+        )).toEqual(selection(20, 10));
+      });
+      test('insert after backwards selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [insertText(25)],
+        )).toEqual(selection(20, 10));
+      });
+      test('insert before multinode selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [1, 0], [4, 0]),
+          [insertText(5, [1, 0])],
+        )).toEqual(selection(15, 20, [1, 0], [4, 0]));
+      });
+      test('insert after multinode selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [1, 0], [4, 0]),
+          [insertText(15, [1, 0])],
+        )).toEqual(selection(10, 20, [1, 0], [4, 0]));
+      });
+      test('insert before multinode selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [1, 0], [4, 0]),
+          [insertText(15, [4, 0])],
+        )).toEqual(selection(10, 25, [1, 0], [4, 0]));
+      });
+      test('insert after multinode selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [1, 0], [4, 0]),
+          [insertText(25, [4, 0])],
+        )).toEqual(selection(10, 20, [1, 0], [4, 0]));
+      });
+      test('insert before backwards multinode selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [4, 0], [1, 0]),
+          [insertText(5, [4, 0])],
+        )).toEqual(selection(15, 20, [4, 0], [1, 0]));
+      });
+      test('insert after backwards multinode selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [4, 0], [1, 0]),
+          [insertText(15, [4, 0])],
+        )).toEqual(selection(10, 20, [4, 0], [1, 0]));
+      });
+      test('insert before backwards multinode selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [4, 0], [1, 0]),
+          [insertText(15, [1, 0])],
+        )).toEqual(selection(10, 25, [4, 0], [1, 0]));
+      });
+      test('insert after backwards multinode selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [4, 0], [1, 0]),
+          [insertText(25, [1, 0])],
+        )).toEqual(selection(10, 20, [4, 0], [1, 0]));
+      });
+    });
+
+    describe('remove_text', () => {
+      test('remove before selection position', () => {
+        expect(slateType.transformSelection(
+          selection(10, 10),
+          [removeText(5)],
+        )).toEqual(selection(5, 5));
+      });
+      test('remove at selection position', () => {
+        expect(slateType.transformSelection(
+          selection(10, 10),
+          [removeText(10)],
+        )).toEqual(selection(10, 10));
+      });
+      test('remove after selection position', () => {
+        expect(slateType.transformSelection(
+          selection(10, 10),
+          [removeText(15)],
+        )).toEqual(selection(10, 10));
+      });
+      test('remove before selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [removeText(5)],
+        )).toEqual(selection(5, 15));
+      });
+      test('remove at selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [removeText(10)],
+        )).toEqual(selection(10, 15));
+      });
+      test('remove between selection range anchor and focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [removeText(12)],
+        )).toEqual(selection(10, 15));
+      });
+      test('remove at selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [removeText(20)],
+        )).toEqual(selection(10, 20));
+      });
+      test('remove after selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [removeText(25)],
+        )).toEqual(selection(10, 20));
+      });
+      test('remove text from the beginning of the range', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [removeText(8)],
+        )).toEqual(selection(8, 15));
+      });
+      test('remove text from the end of the range', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20),
+          [removeText(18)],
+        )).toEqual(selection(10, 18));
+      });
+      test('remove before backwards selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [removeText(5)],
+        )).toEqual(selection(15, 5));
+      });
+      test('remove at backwards selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [removeText(10)],
+        )).toEqual(selection(15, 10));
+      });
+      test('remove between backwards selection range focus and anchor', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [removeText(12)],
+        )).toEqual(selection(15, 10));
+      });
+      test('remove at backwards selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [removeText(20)],
+        )).toEqual(selection(20, 10));
+      });
+      test('remove after backwards selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(20, 10),
+          [removeText(25)],
+        )).toEqual(selection(20, 10));
+      });
+      test('remove before multinode selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [1, 0], [4, 0]),
+          [removeText(5, [1, 0])],
+        )).toEqual(selection(5, 20, [1, 0], [4, 0]));
+      });
+      test('remove after multinode selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [1, 0], [4, 0]),
+          [removeText(15, [1, 0])],
+        )).toEqual(selection(10, 20, [1, 0], [4, 0]));
+      });
+      test('remove before multinode selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [1, 0], [4, 0]),
+          [removeText(15, [4, 0])],
+        )).toEqual(selection(10, 15, [1, 0], [4, 0]));
+      });
+      test('remove after multinode selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [1, 0], [4, 0]),
+          [removeText(25, [4, 0])],
+        )).toEqual(selection(10, 20, [1, 0], [4, 0]));
+      });
+      test('remove before backwards multinode selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [4, 0], [1, 0]),
+          [removeText(5, [4, 0])],
+        )).toEqual(selection(5, 20, [4, 0], [1, 0]));
+      });
+      test('remove after backwards multinode selection range focus', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [4, 0], [1, 0]),
+          [removeText(15, [4, 0])],
+        )).toEqual(selection(10, 20, [4, 0], [1, 0]));
+      });
+      test('remove before backwards multinode selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [4, 0], [1, 0]),
+          [removeText(15, [1, 0])],
+        )).toEqual(selection(10, 15, [4, 0], [1, 0]));
+      });
+      test('remove after backwards multinode selection range anchor', () => {
+        expect(slateType.transformSelection(
+          selection(10, 20, [4, 0], [1, 0]),
+          [removeText(25, [1, 0])],
+        )).toEqual(selection(10, 20, [4, 0], [1, 0]));
+      });
+    });
+  });
+
+  describe('node operations', () => {
+
+    describe('split_node', () => {
+
+    });
+
+    describe('insert_node', () => {
+
+    });
+
+    describe('remove_node', () => {
+
+    });
+
+    describe('merge_node', () => {
+
+    });
+  });
+});
